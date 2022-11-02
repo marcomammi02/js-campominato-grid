fa7eda32eed5928d46a04185d830834bad369909
marcomammi02 <marcomammi2002@gmail.com>
2022-11-02 16:28:06 +0100

    Adding cell logic

---
+++ script.js
@@ -0,0 +1,15 @@
+const eleBtnPlay = document.querySelector('.btn-play');
+const eleTable = document.querySelector('.table');
+
+eleBtnPlay.addEventListener('click', function() {
+    for (let i = 1; i <= 100; i++) {
+        const eleCell = document.createElement('div');
+        eleCell.classList.add('cell');
+        eleCell.innerHTML += i;
+        eleTable.append(eleCell);
+
+        eleCell.addEventListener('click', function() {
+            eleCell.classList.toggle('active');
+        })
+    }
+})
