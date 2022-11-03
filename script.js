5747e45a960f91ababbd4cd7940fdd09939a8482
marcomammi02 <marcomammi2002@gmail.com>
2022-11-03 11:08:18 +0100

    Adding levels

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const eleBtnPlay = document.querySelector('.btn-play');
+const eleLevelSelector = document.querySelector('#level-selector');
 const eleTable = document.querySelector('.table');
+const eleStartTitle = document.querySelector('.start-title');
+const eleLevelNotice = document.querySelector('.level-notice');
 
 eleBtnPlay.addEventListener('click', function() {
-    eleTable.innerHTML = '';
-    eleTable.classList.add('show');
+    eleStartTitle.classList.add('hidden');
+    
+    if (eleLevelSelector.value == '') {
+        eleLevelNotice.classList.remove('hidden');
+        eleTable.classList.add('hidden');
+    } else {
+        eleLevelNotice.classList.add('hidden');
+        eleTable.innerHTML = '';
+        eleTable.classList.remove('hidden');
 
-    for (let i = 1; i <= 100; i++) {
-        const eleCell = document.createElement('div');
-        eleCell.classList.add('cell');
-        eleCell.innerHTML += i;
-        eleTable.append(eleCell);
+        let nCell = parseInt(eleLevelSelector.value);
+        let cellSideDimension = Math.sqrt(nCell);
 
-        eleCell.addEventListener('click', function() {
-            eleCell.classList.toggle('active');
-            console.log('Hai selezionato la casella: ' + i)
-        })
+        for (let i = 1; i <= nCell; i++) {
+            const eleCell = document.createElement('div');
+            eleCell.classList.add('cell');
+            eleCell.style.width = `calc(100% / ${cellSideDimension})`
+            eleCell.style.height = `calc(100% / ${cellSideDimension})`
+            eleCell.innerHTML += i;
+            eleTable.append(eleCell);
+
+            eleCell.addEventListener('click', function() {
+                eleCell.classList.toggle('active');
+                console.log('Hai selezionato la casella: ' + i)
+            })
+        }
     }
+    
 })
